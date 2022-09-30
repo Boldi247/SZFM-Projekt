8dd3308f329b65f5b80cf21cc16564775929306a
lipeczpeti <lipecz.p@gmail.com>
2022-09-30 16:13:33 +0200

    MySQL adatbázis

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,5 +1,91 @@
 package controller;
 
+import exceptions.EmailNotUniqueException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import service.AuthenticationService;
+import service.MysqlService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
 
+    private MysqlService mysqlService = new MysqlService();
+
+    @FXML
+    private AnchorPane globalStage;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField loginUsernameField;
+
+    @FXML
+    private PasswordField loginPasswordField;
+
+    public void onLogin() {
+        mysqlService.connectToServer();
+        if (mysqlService.login(loginUsernameField.getText(), loginPasswordField.getText())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("SIKERES BEJELENTKEZÉS");
+            alert.show();
+
+            showLoggedInScreen();
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("SIKERTELEN BEJELENTKEZÉS");
+            alert.show();
+        }
+        mysqlService.closeConnection();
+    }
+
+    public void onRegistration() {
+        mysqlService.connectToServer();
+        try {
+            mysqlService.registration(username.getText(), password.getText(), email.getText());
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText("SIKERES REGISZTRÁCIÓ");
+            alert.show();
+
+        } catch (EmailNotUniqueException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("EMAIL CÍM NEM EGYEDI!");
+            alert.show();
+        }
+        mysqlService.closeConnection();
+    }
+
+    private void showLoggedInScreen() {
+        Stage stage = (Stage) globalStage.getScene().getWindow();
+
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/opening_scene.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Rezsi kalkulátor");
+        Scene primaryScene = new Scene(root);
+        stage.setScene(primaryScene);
+        stage.show();
+    }
 }
